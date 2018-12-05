151fe1a33dc2deef322736feb48587d3828cf8c3
Pierre Etchemaite <petchema@concept-micro.com>
2018-12-05 01:16:58 +0100

    Revert "Always present popup DaggerfallMessageBox for 1/12 of a second"
    This reverts commit 6b0bba343e3176619b3cbd476f20fc724075b57d.

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallMessageBox.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallMessageBox.cs
@@ -27,7 +27,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
     public class DaggerfallMessageBox : DaggerfallPopupWindow
     {
         const string buttonsFilename = "BUTTONS.RCI";
-        const float minTimePresented = 0.0833f;
 
         Panel imagePanel = new Panel();
         Panel messagePanel = new Panel();
@@ -40,7 +39,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
         bool clickAnywhereToClose = false;
         DaggerfallMessageBox nextMessageBox;
         int customYPos = -1;
-        float presentationTime = 0;
 
         KeyCode extraProceedBinding = KeyCode.None;
 
@@ -217,7 +215,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 Setup();
 
             uiManager.PushWindow(this);
-            presentationTime = Time.realtimeSinceStartup;
         }
 
         public override void Update()
@@ -401,11 +398,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 
         private void ParentPanel_OnMouseClick(BaseScreenComponent sender, Vector2 position)
         {
-            // Must be presented for minimum time before allowing to click through
-            // This prevents capturing parent-level click events and closing immediately
-            if (Time.realtimeSinceStartup - presentationTime < minTimePresented)
-                return;
-
             if (uiManager.TopWindow == this)
             {
                 if (nextMessageBox != null)
