be4a26bef0ec67619aeab8829535bdef2a4344bc
Pierre Etchemaite <petchema@concept-micro.com>
2018-12-05 01:16:40 +0100

    activate objects upon mouse button release
    Avoids issue with click getting thru to the interface
    https://forums.dfworkshop.net/viewtopic.php?f=24&t=1153
    https://forums.dfworkshop.net/viewtopic.php?f=28&t=1361
    https://forums.dfworkshop.net/viewtopic.php?f=24&t=1568

--- Assets/Scripts/Game/PlayerActivate.cs
+++ Assets/Scripts/Game/PlayerActivate.cs
@@ -128,7 +128,7 @@ namespace DaggerfallWorkshop.Game
             }
 
             // Fire ray into scene
-            if (InputManager.Instance.ActionStarted(InputManager.Actions.ActivateCenterObject))
+            if (InputManager.Instance.ActionComplete(InputManager.Actions.ActivateCenterObject))
             {
                 // TODO: Clean all this up
 
